66b25f6c4144d73345ad358c93b8efb8d1996996
Jurak <Ashley_Jurak1@baylor.edu>
2020-10-12 17:48:41 -0500

    checking edges for aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             
     def _check_events(self):
@@ -71,6 +72,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        #update position of all aliens in fleet after checking if it hits the edge
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         #create fleet of aliens
 
@@ -99,6 +105,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        #respond if aliens hit edge
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        #drop entire fleet and change direction
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
